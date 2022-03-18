3d5a0ee21664ffe74a699dae5bda94c55a1881a0
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-19 03:33:04 +0800

    Update urls.py

--- gotspaceanot/urls.py
+++ gotspaceanot/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.welcome, name = 'gotspaceanot-welcome'),
     path('home/', views.home, name = 'gotspaceanot-home'),
     path('about/', views.about, name='gotspaceanot-about'),
+    path('login/', views.login, name = 'gotspaceanot-login'),
 ]
