2a75c4a8a446478c49105419a2af1e8769373d10
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-19 02:25:00 +0800

    Update urls.py

--- gotspaceanot/urls.py
+++ gotspaceanot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'GotSpaceAnot-home'),
-    path('about/', views.about, name='GotSpaceAnot-about'),
+    path('', views.home, name = 'gotspaceanot-home'),
+    path('about/', views.about, name='gotspaceanot-about'),
 ]
