5c4fde24bfe256c6b2d507346d48888bf4fe3869
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-18 14:48:41 +0800

    Update settings.py

--- gotspaceanottest1/settings.py
+++ gotspaceanottest1/settings.py
@@ -133,9 +133,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 
-django_heroku.settings(locals())
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
