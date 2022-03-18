c5d28f927a60892f8725b9e10c19eb6590f5acea
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-18 16:04:59 +0800

    Update settings.py

--- gotspaceanottest1/settings.py
+++ gotspaceanottest1/settings.py
@@ -64,10 +64,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'gotspaceanottest1.urls'
 
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [gotspaceanot.templates,],
+        'DIRS': [TEMPLATE_DIRS,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
