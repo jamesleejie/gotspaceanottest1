0283d266608a65f75ba229eb63b53fd835e6ccb5
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-19 17:38:17 +0800

    Add files via upload

---
+++ sql/wheregotspaceavailable.sql
@@ -0,0 +1,15 @@
+/***************************
+
+  Populate the available table
+
+****************************/
+
+INSERT INTO available VALUES('CLB',1,0,0);
+INSERT INTO available VALUES('CLB',2,0,0);
+INSERT INTO available VALUES('CLB',3,50,50);
+INSERT INTO available VALUES('CLB',4,60,60);
+INSERT INTO available VALUES('CLB',5,70,70);
+INSERT INTO available VALUES('CLB',6,80,80);
+INSERT INTO available VALUES('SLB',4,60,60);
+INSERT INTO available VALUES('SLB',5,70,70);
+INSERT INTO available VALUES('SLB',6,80,80);
