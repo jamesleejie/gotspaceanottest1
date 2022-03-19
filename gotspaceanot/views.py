b802c00fb2cd614b2f84a34e77c6ad612375a7a9
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-19 22:34:39 +0800

    Update views.py

--- gotspaceanot/views.py
+++ gotspaceanot/views.py
@@ -31,3 +31,27 @@ def about(request):
 
 def login(request):
     return render(request, 'gotspaceanot/login.html')
+def add(request):
+    """Shows the main page"""
+    context = {}
+    status = ''
+
+    if request.POST:
+        ## Check if customerid is already in the table
+        with connection.cursor() as cursor:
+
+            cursor.execute("SELECT * FROM student WHERE student = %s", [request.POST['student']])
+            student = cursor.fetchone()
+            ## No customer with same id
+            if student == None:
+                ##TODO: date validation
+                cursor.execute("INSERT INTO student VALUES (%s, %s)"
+                        , [request.POST['Matric Number'], request.POST['Email'] ])
+                return redirect('index')    
+            else:
+                status = 'Student with Matric Number %s already exists' % (request.POST['student'])
+
+
+    context['status'] = status
+ 
+    return render(request, "gotspaceanot/add.html", context)
