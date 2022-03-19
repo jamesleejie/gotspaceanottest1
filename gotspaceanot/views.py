a804777acb89033a908534a97c6f71afcf34b0b4
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-19 20:32:10 +0800

    Update views.py

--- gotspaceanot/views.py
+++ gotspaceanot/views.py
@@ -3,34 +3,18 @@ from django.db import connection
 
 # Create your views here.
 
-library_system = [
-    {
-        'Library': 'CLB',
-        'Level': '3',
-        'Available_Space': '50',
-        'Total_Space': '50',  
-    },
-    {
-        'Library': 'SLB',
-        'Level': '4',
-        'Available_Space': '20',
-        'Total_Space': '20',  
-    }
-]
-
 def welcome(request): 
     return render(request, "gotspaceanot/welcome.html")
 
 def home(request):
     """Shows the main page"""    
-    context = {
-        'library_system': library_system
-    }
+    
     ## Use raw query to get all objects
     with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM context ORDER BY Library")
-        library = cursor.fetchall()    
-    return render(request, 'gotspaceanot/home.html', context)
+        cursor.execute("SELECT * FROM available ORDER BY library")
+    result_dict = {'records': available}
+
+    return render(request, 'gotspaceanot/home.html', result_dict)
 
 def about(request):
     return render(request, 'gotspaceanot/about.html')
