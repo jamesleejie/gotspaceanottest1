147c627a002ae349641594ae3f3fd035d82158ae
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-19 23:32:01 +0800

    Update wheregotspacelibrary_system.sql

--- sql/wheregotspacelibrary_system.sql
+++ sql/wheregotspacelibrary_system.sql
@@ -6,3 +6,5 @@
 
 INSERT INTO library_system VALUES('A0217795N','abs@gmail.com','CLB');
 INSERT INTO library_system VALUES('A1234567N','efg@gmail.com','SLB');
+INSERT INTO library_system VALUES('A8765432P','A8765432P@u.nus.edu','SLB');
+
