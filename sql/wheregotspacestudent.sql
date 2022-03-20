6466868ab47ef011808eeaa0c6ef00cb179a420e
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-20 14:52:48 +0800

    Update wheregotspacestudent.sql

--- sql/wheregotspacestudent.sql
+++ sql/wheregotspacestudent.sql
@@ -4,5 +4,5 @@
 
 ****************************/
 
-INSERT INTO student VALUES('A0217795N','abs@gmail.com');
-INSERT INTO student VALUES('A1234567N','efg@gmail.com');
+INSERT INTO student VALUES('A0217795N','abs@gmail.com', 'CLB', 4);
+INSERT INTO student VALUES('A1234567N','efg@gmail.com', 'SLB', 4);
