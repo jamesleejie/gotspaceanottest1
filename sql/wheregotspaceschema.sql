005623c0cd480d620eeb4578c7bc4cdacc61506d
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-03-20 14:54:01 +0800

    Update wheregotspaceschema.sql

--- sql/wheregotspaceschema.sql
+++ sql/wheregotspaceschema.sql
@@ -7,7 +7,7 @@
 CREATE TABLE IF NOT EXISTS library_system (
   matric_number VARCHAR(9) NOT NULL ,
   email VARCHAR(256) NOT NULL,
-  library VARCHAR(7) NOT NULL, CHECK (library IN ('CLB','SLB')),
+  library VARCHAR(7) NOT NULL
   time_entered TIMESTAMPTZ DEFAULT Now(),
   time_exited TIMESTAMPTZ ,
   PRIMARY KEY(matric_number,email) );  --So that student can have multiple entries in the system where they entered at different times
@@ -15,6 +15,8 @@ CREATE TABLE IF NOT EXISTS library_system (
 CREATE TABLE IF NOT EXISTS student(
   matric_number VARCHAR(9),
   email VARCHAR(256),
+  library VARCHAR(7) NOT NULL,
+  Level INT NOT NULL,
   FOREIGN KEY (matric_number, email) REFERENCES library_system(matric_number,email)
   ON DELETE CASCADE DEFERRABLE
 );
