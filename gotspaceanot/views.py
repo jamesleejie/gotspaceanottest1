e53ba9c68d7a9128a500960c91f09e525ec8335c
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-01 15:30:52 +0800

    Update views.py

--- gotspaceanot/views.py
+++ gotspaceanot/views.py
@@ -81,7 +81,7 @@ def logout(request):
             else:
                 ##Updating the available space when a student logsout
                 cursor.execute("UPDATE available SET available_seats = available_seats + 1 WHERE (library,Level) = (%s, %s)", [student[2] , student[3]])
-                cursor.execute("DELETE FROM student WHERE matric_number = (%s)", [request.POST['Matric Number']])
+                cursor.execute("UPDATE student SET time_exited = Now() WHERE matric_number = (%s)", [request.POST['Matric Number']])
                 cursor.execute("UPDATE library_system SET time_exited = Now() WHERE matric_number = (%s)", [request.POST['Matric Number']])
                 
                 return redirect('gotspaceanot-welcome') 
