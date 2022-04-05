af8f1c10fc888eb24dc5da3fb6b501ee0c8522f8
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-05 12:17:36 +0800

    Update urls.py

--- gotspaceanot/urls.py
+++ gotspaceanot/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name = 'gotspaceanot-welcome'),
     path('library_system_records/', views.library_system_records, name = 'gotspaceanot-library_system_records'),
+    path('nus_system/', views.nus_system, name = 'gotspaceanot-nus_system'),    
     path('about/', views.about, name='gotspaceanot-about'),
     path('login/', views.login, name = 'gotspaceanot-login'),
     path('logout/', views.logout, name = 'gotspaceanot-logout'),    
