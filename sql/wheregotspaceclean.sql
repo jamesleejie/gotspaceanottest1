a573d33b04d92313af3bbd29e7441daf1493a328
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-07 01:18:22 +0800

    Update wheregotspaceclean.sql

--- sql/wheregotspaceclean.sql
+++ sql/wheregotspaceclean.sql
@@ -4,7 +4,10 @@
 
 *******************/
 
+
 DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS library_system;
 DROP TABLE IF EXISTS available;
 DROP TABLE IF EXISTS NUS_system;
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS faculty;
