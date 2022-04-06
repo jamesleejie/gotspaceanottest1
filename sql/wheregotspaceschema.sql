822c0904e9ad8450caf83c7b8d3e81b0e8620e7f
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-06 19:09:29 +0800

    Update wheregotspaceschema.sql

--- sql/wheregotspaceschema.sql
+++ sql/wheregotspaceschema.sql
@@ -18,6 +18,16 @@ CREATE TABLE IF NOT EXISTS library_system (
   PRIMARY KEY(matric_number,email,time_entered)
 );  --So that student can have multiple entries in the system where they entered at different times
 
+
+CREATE TABLE IF NOT EXISTS available(
+	library VARCHAR(7) NOT NULL,
+	level INT NOT NULL,
+	available_seats INT NOT NULL CHECK(available_seats > -1 and available_seats<=total_seats),
+	total_seats INT NOT NULL,
+	PRIMARY KEY(library, level)
+);
+
+
 CREATE TABLE IF NOT EXISTS student(
   matric_number VARCHAR(9) REFERENCES NUS_system(matric_number),
   email VARCHAR(256) REFERENCES NUS_system(email) ,
@@ -28,10 +38,4 @@ CREATE TABLE IF NOT EXISTS student(
   PRIMARY KEY(matric_number,email,time_entered)
 );
 
-CREATE TABLE IF NOT EXISTS available(
-	library VARCHAR(7) NOT NULL,
-	level INT NOT NULL,
-	available_seats INT NOT NULL CHECK(available_seats > -1 and available_seats<=total_seats),
-	total_seats INT NOT NULL,
-	PRIMARY KEY(library, level)
-);
+
