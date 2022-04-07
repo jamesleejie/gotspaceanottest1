1c6288e509cd50fbf68350c340ab1f3e76316dbf
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-07 21:22:41 +0800

    Update add_batch1.sh

--- add_batch1.sh
+++ add_batch1.sh
@@ -28,4 +28,4 @@ done < ".env"
 URI="postgres://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 
 # Run the scripts to insert data.
-psql ${URI} -f sql/batch1_login.sql
+psql ${URI} -f sql/add_library_system.sql
