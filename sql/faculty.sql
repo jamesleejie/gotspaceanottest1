ea7ed7f0046f3f6ac38e384fe08b4ceba8c9217d
LiaoWenjun99 <101497609+LiaoWenjun99@users.noreply.github.com>
2022-04-07 20:49:22 +0800

    Create faculty.sql

---
+++ sql/faculty.sql
@@ -0,0 +1,13 @@
+insert into faculty (faculty) values ('Science');
+insert into faculty (faculty) values ('Business');
+insert into faculty (faculty) values ('Design & Engineering');
+insert into faculty (faculty) values ('Arts & Social Sciences');
+insert into faculty (faculty) values ('Computing');
+insert into faculty (faculty) values ('Dentistry');
+insert into faculty (faculty) values ('Medicine');
+insert into faculty (faculty) values ('Law');
+
+
+
+
+
